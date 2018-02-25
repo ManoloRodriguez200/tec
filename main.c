e8fc6944c6470448e16ad19b8753af2808e94e07
ManoloRodriguez200 <mrodriguez390@gmail.com>
2018-02-25 11:59:13 -0600

    Update main.c

--- main.c
+++ main.c
@@ -11,8 +11,58 @@
 static struct termios initial_settings, new_settings;
 static int peek_character = -1;
 
+// MAIN  --------------------------------------------------------------------------------------
 
-// kbhit INICIO --------------------------------------------------------------------------------------
+int main(int argc, char *argv[]) {
+
+    setlogmask (LOG_UPTO (LOG_DEBUG));
+    openlog(">>rastreador>>", LOG_PID|LOG_CONS, LOG_USER);
+    syslog(LOG_DEBUG, "Iniciando Programa");
+
+    int opt;
+    while((opt = getopt(argc, argv, "-v:V")) != EOF) {
+
+
+        switch (opt) {
+            case 'v':
+                syslog(LOG_DEBUG, "Llamando a funcion de rastreo con -v");
+                trace(argc, argv);
+                break;
+            case 'V':
+                syslog(LOG_DEBUG, "Llamando a funcion de rastreo con -V");
+                puts("Presione cualquier tecla para continuar ...");
+
+                int ch = 0;
+                init_keyboard();
+                while(ch == 0) {
+                    //Monitoreo de terminal en intervalos de 1 segundo.
+                    sleep(1);
+                    if(keyboardInt()) {
+                        ch = readch();
+                        syslog(LOG_DEBUG, "tecla presionada: %c\n",ch); //logueo de informacion
+                        trace(argc, argv);
+                    }
+                }
+                break;
+                //validacion de argumentos y formacion de string/cadena.
+            case ':':
+                usageError(argv[0], "Falta parametro/argumento", optopt);
+            case '?':
+                usageError(argv[0], "Opcion invalida", optopt);
+            default:
+                usageError(argv[0], "Falta parametro/argumento", optopt);
+        }
+
+        syslog(LOG_DEBUG, "opt =%3d (%c); optind = %d", opt, printable(opt), optind);
+        if (opt == '?' || opt == ':') {
+            syslog(LOG_DEBUG, "; optopt =%3d (%c)", optopt, printable(optopt));
+        }
+    }
+    closelog();
+
+    return 0;
+}
+// interacion con el teclado.
 
 void init_keyboard() {
     tcgetattr(0,&initial_settings);
@@ -29,7 +79,7 @@ void close_keyboard() {
     tcsetattr(0, TCSANOW, &initial_settings);
 }
 
-int kbhit() {
+int keyboardInt() {
     unsigned char ch;
     int nread;
 
@@ -61,8 +111,8 @@ int readch() {
 }
 
 
-// kbhit FIN --------------------------------------------------------------------------------------
-
+// keyboardInt End
+//errores de entra por parte de usuario
 static void usageError(char *progName, char *msg, int opt) {
     if (msg != NULL && opt != 0) {
         fprintf(stderr, "%s (-%c)\n", msg, printable(opt));
@@ -73,10 +123,9 @@ static void usageError(char *progName, char *msg, int opt) {
 
 
 /*
- * Esta funcion llama al comando strace para rastrear las llamadas al sistema hechas por un programa. Para obtener
- * una tabla con los acumulados se llama a strace con los parametros -c y -f
+ * Funcion de rastreo usando strace y parametros(-c, -f) extra para mostrar la informacion en una tabla.
  */
-void traceCall(int argc, char *argv[]){
+void trace(int argc, char *argv[]){
 
 
     int initialIndex = 2;
@@ -102,55 +151,3 @@ void traceCall(int argc, char *argv[]){
     arguments[i] = NULL;
 
     execvp("strace", arguments);
-}
-
-// MAIN  --------------------------------------------------------------------------------------
-
-int main(int argc, char *argv[]) {
-
-    setlogmask (LOG_UPTO (LOG_DEBUG));
-    openlog(">>rastreador>>", LOG_PID|LOG_CONS, LOG_USER);
-    syslog(LOG_DEBUG, "Iniciando rastreador");
-
-    int opt;
-    while((opt = getopt(argc, argv, "-v:V")) != EOF) {
-
-
-        switch (opt) {
-            case 'v':
-                syslog(LOG_DEBUG, "Llamando a traceCall con -v");
-                traceCall(argc, argv);
-                break;
-            case 'V':
-                syslog(LOG_DEBUG, "Llamando a traceCall con -V");
-                puts("Presione cualquier tecla para continuar...");
-
-                int ch = 0;
-                init_keyboard();
-                while(ch == 0) {
-                    //Se comienza a monitorear la terminal cada segundo para saber si algo se presiono
-                    sleep(1);
-                    if(kbhit()) {
-                        ch = readch();
-                        syslog(LOG_DEBUG, "tecla presionada: %c\n",ch);
-                        traceCall(argc, argv);
-                    }
-                }
-                break;
-            case ':':
-                usageError(argv[0], "Falta argumento", optopt);
-            case '?':
-                usageError(argv[0], "Opcion invalida", optopt);
-            default:
-                usageError(argv[0], "Falta argumento", optopt);
-        }
-
-        syslog(LOG_DEBUG, "opt =%3d (%c); optind = %d", opt, printable(opt), optind);
-        if (opt == '?' || opt == ':') {
-            syslog(LOG_DEBUG, "; optopt =%3d (%c)", optopt, printable(optopt));
-        }
-    }
-    closelog();
-
-    return 0;
-}
